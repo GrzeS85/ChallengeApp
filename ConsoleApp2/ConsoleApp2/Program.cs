4b321f67db41f6282b2d95e058830e0698e1b03b
Grzegorz Lipiec <g.lipiec.lca@gmail.com>
2024-01-14 18:45:23 +0100

    zadanie domowe dzień 4

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿var name = "Ewa";
+var sex = "women";
+var age = 30;
+
+
+if (sex == "women" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "man" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("Ktos inny");
+}
+
+
+
+
+
